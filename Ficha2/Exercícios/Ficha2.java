2ac23e5527c4cb0e8249b1c7dde6ea1d0e263dbc
Mariana-rc01 <rc.mariana.01@gmail.com>
2024-02-29 12:13:03 +0000

    Added almost every exercises of Ficha2

--- Ficha2/Exercícios/Ficha2.java
+++ Ficha2/Exercícios/Ficha2.java
@@ -1,13 +1,24 @@
+import java.time.Duration;
+import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 
 public class Ficha2 {
     private int[] arr;
+    private LocalDate[] datas;
 
-    public Ficha2(int[] array){
+    private String[] strings;
+
+    public Ficha2(int[] array, LocalDate[] dates, String[] string){
         this.arr = Arrays.copyOfRange(array, 0, array.length);
+        this.datas = Arrays.copyOfRange(dates,0, dates.length);
+        this.strings = Arrays.copyOfRange(string,0,string.length);
     }
 
-    // Exercício 1 a)
+    /* Exercício 1*/
+
+    // Exercício 1a)
     public int valorMinimo(){
         int min = Integer.MAX_VALUE;
         for (int el : arr)
@@ -16,7 +27,7 @@ public class Ficha2 {
         return min;
     }
 
-    // Exercício 1 b)
+    // Exercício 1b)
     public int[] valorEntre(int ind1, int ind2){
         int[] array_return = new int[ind2-ind1+1];
         int j = 0;
@@ -27,7 +38,7 @@ public class Ficha2 {
         return array_return;
     }
 
-    // Exercício 1 c)
+    // Exercício 1c)
     public int[] elementosComuns(int[] array2){
         int[] array_return = new int[arr.length];
         int el = 0;
@@ -46,18 +57,157 @@ public class Ficha2 {
         return Arrays.copyOfRange(array_return,0,el);
     }
 
-    // Exercício 3 a)
+
+    /* Exercício 2*/
+
+    // Exercício 2a)
+    public void insereData(LocalDate data){
+        LocalDate[] newArr = Arrays.copyOf(datas, datas.length + 1);
+        newArr[newArr.length-1] = data;
+        datas = newArr;
+    }
+
+    // Exercício 2b)
+    public LocalDate dataMaisProxima(LocalDate data){
+        long diferenca = 0, duracao = 0;
+        LocalDate maisProxima = datas[0];
+        for (LocalDate d: datas){
+            duracao = Duration.between(data, d).getSeconds();
+            if (Math.abs(duracao) < Math.abs(diferenca)) {
+                diferenca = duracao;
+                maisProxima = d;
+            }
+        }
+        return maisProxima;
+    }
+
+    // Exercício 2c)
+    public String toString(){
+        return Arrays.toString(datas);
+    }
+
+    /* Exercício 3*/
+
+    // Exercício 3a)
     public int[] ordena(){
         Arrays.sort(arr);
         return arr;
     }
 
-    // Exercício 3 b)
-
+    // Exercício 3b)
     public int procura(int i){
         return Arrays.binarySearch(arr,i);
     }
 
-    // Exercício 5
+    /* Exercício 4*/
+
+    // Exercício 4a)
+    public String[] semReps(){
+        HashSet<String> set = new HashSet<>();
+        for(String s: strings){
+            set.add(s);
+        }
+        // Collections.addAll(set, strings); -> outra forma de fazer o for
+        return set.toArray(new String[set.size()]);
+    }
+
+    // Exercicio 4b)
+
+    public String maiorString(){
+        String maior = strings[0];
+        int tamanho = 0;
+        for(String string: strings){
+            if (string.length() > tamanho){
+                tamanho = string.length();
+                maior = string;
+            }
+        }
+        return maior;
+    }
+
+    // Exercício 4c)
+    public String[] apenasReps(){
+        String[] repeticoes = new String[strings.length];
+        Arrays.sort(strings);
+        int tamanho = 0;
+        for (int i = 0; i < strings.length-1; i++){
+            if (strings[i].equals(strings[i+1])) {
+                if (tamanho == 0 || !strings[i].equals(repeticoes[tamanho - 1]))
+                    repeticoes[tamanho++] = strings[i];
+            }
+        }
+        repeticoes = Arrays.copyOf(repeticoes, tamanho);
+        return repeticoes;
+    }
+
+    // Exercício 4d)
+    public int numeroString(String string){
+        int n = 0;
+        for(String s: strings){
+            if (s.equals(string)) n++;
+        }
+        return n;
+    }
+
+    /* Exercício 5*/
+
+
+
+
+    /* Exercício 7*/
+    public int[] geracaoNumeros(){
+        int[] numerosChave = new int[5];
+        int i = 0, numero;
+        while (i != 5){
+            numero = (int) (Math.random() % 50 + 1);
+            boolean repetido = false;
+            for (int j = 0; j < i; j++){
+                if (numerosChave[j] == numero) {
+                    repetido = true;
+                    break;
+                }
+            }
+            if (!repetido){
+                numerosChave[i++] = numero;
+            }
+        }
+        return numerosChave;
+    }
+
+    public int[] geracaoEstrelas(){
+        int[] estrelasChave = new int[2];
+        int i = 0, numero = 0;
+        while (i != 2){
+            numero = (int) (Math.random() % 9 + 1);
+            boolean repetido = false;
+            for (int j = 0; j < i; j++){
+                if (estrelasChave[j] == numero) {
+                    repetido = true;
+                    break;
+                }
+            }
+            if (!repetido){
+                estrelasChave[i++] = numero;
+            }
+        }
+        return estrelasChave;
+    }
+
+    public int[] Euromilhoes(int[] chave, int[] user){
+        int[] array_return = new int[chave.length];
+        int el = 0;
+        for (int i = 0; i < chave.length; i++){
+            for (int j = 0; j < user.length; j++){
+                if(chave[i] == user[j]) {
+                    boolean flag = false;
+                    for(int k = 0; k < el && !flag; k++){ // Verificar se ainda está no result
+                        if (array_return[k] == chave[i]) flag = true;
+                    }
+                    if (!flag) array_return[el++] = chave[i];
+                }
+            }
+        }
+        return Arrays.copyOfRange(array_return,0,el);
+    }
 
 }
--- Ficha2/Exercícios/TestePrograma.java
+++ Ficha2/Exercícios/TestePrograma.java
@@ -1,9 +1,11 @@
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.lang.Math;
 
 public class TestePrograma {
     public static void main(String[] args) throws Exception{
-        System.out.println("Insira o tamanho do array: ");
+        System.out.println("Insira o tamanho do array de inteiros: ");
         Scanner sc = new Scanner(System.in);
 
         int array_size = sc.nextInt();
@@ -15,7 +17,34 @@ public class TestePrograma {
             array[i] = el;
         }
 
-        Ficha2 f2 = new Ficha2(array);
+        System.out.println("Insira o tamanho do array de datas: ");
+        int arrayDT = sc.nextInt();
+
+        LocalDate[] arrayD = new LocalDate[arrayDT];
+
+        for (int i = 0; i < arrayDT; i++) {
+            System.out.println("Insira uma data no formato yyyy-MM-dd: ");
+            String dateString= sc.next();
+            LocalDate date = LocalDate.parse(dateString);
+            arrayD[i] = date;
+        }
+
+        System.out.println("Insira o tamanho do array de strings: ");
+        int arrayST = sc.nextInt();
+
+        String[] arrayS = new String[arrayST];
+
+        for (int i = 0; i < arrayST; i++) {
+            String s = sc.next();
+            arrayS[i] = s;
+        }
+
+        Ficha2 f2 = new Ficha2(array,arrayD,arrayS);
+        String[] repetidas = f2.apenasReps();
+
+        for (String palavra: repetidas){
+            System.out.println(palavra);
+        }
         System.out.println("Introduza o número da pergunta: ");
         int pergunta = sc.nextInt();
 
@@ -48,6 +77,61 @@ public class TestePrograma {
                     int el = sc.nextInt();
                     array2[i] = el;
                 }
+                break;
+            }
+            case 7:{
+
+                int[] numerosChave;
+                int[] estrelasChave;
+
+                numerosChave = Arrays.copyOfRange(f2.geracaoNumeros(),0,5);
+                estrelasChave = Arrays.copyOfRange(f2.geracaoEstrelas(),0,2);
+
+                System.out.println("Insira 5 numeros para o EuroMilhoes: ");
+                int[] arrayN = new int[5];
+                for (int i = 0; i < 5; i++) {
+                    int el = sc.nextInt();
+                    arrayN[i] = el;
+                }
+
+                System.out.println("Insira 2 estrelas para o EuroMilhoes: ");
+                int[] arrayE = new int[2];
+                for (int i = 0; i < 2; i++) {
+                    int el = sc.nextInt();
+                    arrayE[i] = el;
+                }
+
+                int[] repetidosNumero = f2.Euromilhoes(numerosChave,arrayN);
+                int[] repetidosEstrela = f2.Euromilhoes(estrelasChave,arrayE);
+
+                if (repetidosEstrela.length == 2 && repetidosNumero.length == 5){
+                    System.out.println("Acertou na chave!!!");
+                    for (int i = 0; i < 50; i++){
+                        for (int numero: numerosChave)
+                            System.out.print(numero + " ");
+
+                        for (int estrela: estrelasChave)
+                            System.out.print(estrela + " ");
+
+                        System.out.println();
+
+                        for (int j = 0; j < i+1; j++){
+                            System.out.print("  ");
+                        }
+                    }
+                }
+                else {
+                    System.out.println("Numeros coincidentes: ");
+                    for (int j : repetidosNumero) {
+                        System.out.println(j);
+                    }
+
+                    System.out.println("Estrelas coincidentes: ");
+                    for (int k : repetidosEstrela) {
+                        System.out.println(k);
+                    }
+                }
+
             }
         }
 
